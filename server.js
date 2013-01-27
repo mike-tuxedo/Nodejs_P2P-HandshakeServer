4c817710a12f2d517db3b9aa50310179d9222e83
Franz Josef <franz.josef.bruenner@gmail.com>
2013-01-27 10:34:59 +0100

    trace method added in server_methods.js, client-JSON-Object created for associating user-id's with user-server-connections

--- server.js
+++ server.js
@@ -3,33 +3,35 @@ var serverMethods = require('./server_methods');
 // websocket-server and clients
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({port: '9005'});
-var clients = [];
+
+// to hold all user-connections
+var sockets = [];
 
 wss.on('connection', function(ws) {
     
-    console.log('client connected');
-    
     if( serverMethods.isValidOrigin(ws) ){
-      clients.push(ws);
+      serverMethods.trace('client connected');
+      sockets.push(ws);
     }
     else{
       return;
     }
     
+    
+    /* message-kinds (from client to server): */
+    //// register (new User or Guest) -> { init: true, url: 'www.example.at/#...' }
+    //// spd/ice -> { chatroomHash: '...', userHash: '...', destinationHash: '...', spd or ice: Object }
+    
+    /* message-kinds (from server to client): */
+    //// register (new User or Guest) -> { init: true, chatroom: '...', userID: '...', guestIds: [{id '...'},...] }
+    //// spd/ice -> { chatroomHash: '...', userHash: '...', spd or ice: Object }
+    
     ws.on('message', function(message) {
     
-      /* message-kinds (from client to server): */
-      //// register (new User or Guest) -> { init: true, url: 'www.example.at/#...' }
-      //// spd/ice -> { chatroomHash: '...', userHash: '...', destinationHash: '...', spd or ice: Object }
-      
-      /* message-kinds (from server to client): */
-      //// register (new User or Guest) -> { init: true, chatroom: '...', userID: '...', guestIds: [{id '...'},...] }
-      //// spd/ice -> { chatroomHash: '...', userHash: '...', spd or ice: Object }
-      
       message = JSON.parse(message);
       
       if( serverMethods.isRegisterMessage(message) ){
-        var newUser = clients[clients.length-1];
+        var newUser = sockets[sockets.length-1];
         serverMethods.setupNewUser(newUser, message.url);
       }
       
--- server_methods.js
+++ server_methods.js
@@ -2,13 +2,15 @@
 var hash = require('./hash');
 var properties = require('./properties');
 
+// to associate user-id's with user-connection's
+exports.clients = {};
+
 exports.isValidOrigin = function(req){ // client must have got a certain domain in order to proceed
   if(req.upgradeReq.headers.origin == properties.phovecUrl)
     return true;
   else
     return false;
 };
-
 
 exports.isRegisterMessage = function(msg){
   return msg.init;
@@ -21,16 +23,26 @@ exports.setupNewUser = function(socket,clientUrl){ // user gets handled by wheth
     function(clientInfo){
     
       if( clientInfo.success ){ // if true user can build chatroom or enter already created chatroom
+        
+        exports.clients[clientInfo.userHash] = socket;
+        
         socket.send(JSON.stringify({ // server informs user about chatroom-hash and userID's
           init: true, 
           chatroom: clientInfo.roomHash, 
           userID: clientInfo.userHash, 
           guestIds: clientInfo.guestIds 
         }));
+        
       }
       else
         socket.send(JSON.stringify(clientInfo));
     }
   );
   
+};
+
+exports.trace = function(msg){
+  console.log('---------------------------------------------------------------');
+  console.log(msg);
+  console.log('---------------------------------------------------------------');
 };
