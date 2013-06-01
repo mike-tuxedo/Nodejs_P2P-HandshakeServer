ffdd3929b40299583577ee4955d534e03ed04d13
Franz Josef <franz.josef.bruenner@gmail.com>
2013-06-01 11:08:11 +0200

    helpers-test debugged and tested

--- tests/spec/helpers/helpers_test.spec.js
+++ tests/spec/helpers/helpers_test.spec.js
@@ -8,52 +8,32 @@ var socket = {};
 socket._socket = {};
 socket._socket.remoteAddress = '178.194.43.211'; // all guest users share same ip-address
 
-var firstTestPassed = false;
-
-// simulates init:user messages
-beforeEach(function (done) {
-  if(firstTestPassed){
-    var userToInsert = properties.usersToInsert.shift();
-    
-    mongodb.insertUser(
-      properties.chatroomHash, 
-      userToInsert.id, 
-      userToInsert.name,
-      function(){
-        console.log('userToInsert: ',userToInsert);
-        done();
-      }
-    );
-  }
-  else{
-    done();
-  }
-});
-
 describe('a guest is invited and comes in a chatroom', function() {
   
-  it('userId-Array should contain host-id', function(done) {
-    
+  it('query should contain host-user', function(done) {
+  
     helpers.handleClient(
       socket, 
       (hostUrl + '/' + properties.chatroomHash),
       function(infoForClient){
       
       var users = infoForClient.users;
+      
       expect(properties.hostUserHash).toEqual(users[0].id);
       expect(properties.hostUserName).toEqual(users[0].name);
-      done();
+      expect(1).toEqual(users.length);
+      
+      insertNextUser(done);
       
     });
     
   });
   
-  it('userId-Array should contain two id\'s: host and guest of users', function(done) {
-    
+  it('query should contain two user\'s: host and first guest', function(done) {
+  
     helpers.handleClient(
       socket,
       (hostUrl + '/' + properties.chatroomHash),
-      properties.thirdUserName,
       function(infoForClient){
       
       var users = infoForClient.users;
@@ -61,30 +41,47 @@ describe('a guest is invited and comes in a chatroom', function() {
       expect(properties.hostUserHash).toEqual(users[0].id);
       expect(properties.secondUserHash).toEqual(users[1].id);
       expect(properties.secondUserName).toEqual(users[1].name);
-      done();
+      expect(2).toEqual(users.length);
+      
+      insertNextUser(done);
       
     });
     
   });
   
-  it('userId-Array should contain three id\'s', function(done) {
-    
+  it('query should contain three user\'s', function(done) {
+  
     helpers.handleClient(
       socket,
       (hostUrl + '/' + properties.chatroomHash), 
-      properties.fourthUserName,
       function(infoForClient){
-    
+      
       var users = infoForClient.users;
       
       expect(properties.hostUserHash).toEqual(users[0].id);
       expect(properties.thirdUserHash).toEqual(users[2].id);
       expect(properties.thirdUserName).toEqual(users[2].name);
       expect(3).toEqual(users.length);
-      done();
+      
+      insertNextUser(done);
       
     });
     
   });
   
 });
+
+function insertNextUser(done){
+  
+  var userToInsert = properties.usersToInsert.shift();
+      
+  mongodb.insertUser(
+    properties.chatroomHash, 
+    userToInsert.id, 
+    userToInsert.name, 
+    'AT', 
+  function(){
+    done();
+  });
+
+}
